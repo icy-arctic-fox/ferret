0704951b288d381f6789a174076360f12543d761
Michael Miller <icy.arctic.fox@gmail.com>
2018-01-31 14:14:12 -0700

    Add abstract helper to simulate abstract methods

--- lib/ferret.rb
+++ lib/ferret.rb
@@ -1,5 +1,7 @@
-require 'ferret/version'
+require_relative 'ferret/version'
 
 module Ferret
   # Your code goes here...
 end
+
+require_relative 'ferret/abstract'
---
+++ lib/ferret/abstract.rb
@@ -0,0 +1,13 @@
+class Class
+  # Creates a place-holder method that is expected to be overridden.
+  # This simulates having abstract methods in Ruby.
+  # If the method is not overridden by a sub-class and the method is called,
+  # then a +NoMethodError+ is raised.
+  # @param method [Symbol] Name of the method to define.
+  # @return [void]
+  def abstract(method)
+    define_method(method) do
+      raise NoMethodError, "Unimplemented abstract method #{self.class}##{method} called"
+    end
+  end
+end
