5023fcd0ef6e89cbed0083ce88ab1f1af172c366
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-01 15:37:33 -0700

    Implement and test FileDiff class
    Add file attribute to file diff.

--- lib/ferret/file_diff.rb
+++ lib/ferret/file_diff.rb
@@ -1,6 +1,10 @@
 module Ferret
   # Collects all known differences between two files.
   class FileDiff
+    # File the diff was performed on.
+    # @return [File]
+    attr_reader :file
+
     # New lines that were added to the file.
     # @return [Array<SourceLine>]
     attr_reader :lines_added
@@ -8,5 +12,16 @@ module Ferret
     # Old lines that were removed from the file.
     # @return [Array<SourceLine>]
     attr_reader :lines_removed
+
+    # Creates a file diff.
+    # @param file [File] File the diff was performed on.
+    # @param lines_added [Enumerable<SourceLine>] Set of lines that were added to the file.
+    # @param lines_removed [Enumerable<SourceLine>] Set of lines that were removed from the file.
+    def initialize(file, lines_added = [], lines_removed = [])
+      @file          = file
+      @lines_added   = lines_added.to_a.dup.freeze
+      @lines_removed = lines_removed.to_a.dup.freeze
+      freeze
+    end
   end
 end
---
+++ spec/file_diff_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe Ferret::FileDiff do
+  subject(:file_diff) { build(:file_diff) }
+
+  describe '#file' do
+    let(:file) { build(:file) }
+    subject(:file_diff) { build(:file_diff, file: file) }
+    subject { file_diff.file }
+
+    it 'is the expected value' do
+      is_expected.to eq(file)
+    end
+  end
+
+  describe '#lines_added' do
+    let(:lines_added) { build_list(:source_line, 5) }
+    subject(:file_diff) { build(:file_diff, lines_added: lines_added) }
+    subject { file_diff.lines_added }
+
+    it 'is the expected value' do
+      is_expected.to eq(lines_added)
+    end
+
+    it 'is immutable' do
+      expect { subject << build(:source_line) }.to raise_error(RuntimeError, /frozen/)
+    end
+  end
+
+  describe '#lines_removed' do
+    let(:lines_removed) { build_list(:source_line, 5) }
+    subject(:file_diff) { build(:file_diff, lines_removed: lines_removed) }
+    subject { file_diff.lines_removed }
+
+    it 'is the expected value' do
+      is_expected.to eq(lines_removed)
+    end
+
+    it 'is immutable' do
+      expect { subject << build(:source_line) }.to raise_error(RuntimeError, /frozen/)
+    end
+  end
+
+  it 'is frozen' do
+    is_expected.to be_frozen
+  end
+end
