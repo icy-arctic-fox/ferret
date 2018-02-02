623c5eb9354d13e96fe0ca5fdd9111c63bedf1b1
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-02 15:31:18 -0700

    Some initial work on the git driver

--- lib/ferret/git/git_vcs_driver.rb
+++ lib/ferret/git/git_vcs_driver.rb
@@ -1,9 +1,90 @@
+require 'rugged'
+require 'digest'
 require_relative '../vcs_driver'
 
 module Ferret
   module Git
     # Implementation details for operating on a git repository.
     class GitVcsDriver < VcsDriver
+      # Creates a new driver for git repositories.
+      # @param cache [RepositoryCache] Cache to track which repositories are actively being used.
+      def initialize(cache)
+        @cache = cache
+        freeze
+      end
+
+      # Retrieves the file tree of a repository at a specified revision.
+      # @param revision [RevisionId] Revision to get the tree structure at.
+      # @return [FileTree]
+      def file_tree_at_revision(revision)
+        raise NotImplementedError
+      end
+
+      # Retrieves the raw file contents of a file at a specified revision.
+      # This method can accept a block.
+      # When a block is provided, the stream will automatically be disposed when the block returns.
+      # If a block isn't provided, then an +IO+ instance will be returned that contains the same stream.
+      # @param revision [RevisionId] Revision to get the files contents at.
+      # @param file [File] File to retrieve the contents of.
+      # @return [IO] Stream of the file contents, if no block was provided.
+      # @return [void] Block was provided to the method.
+      # @yieldparam io [IO] Stream of the file contents.
+      def file_contents_at_revision(revision, file)
+        raise NotImplementedError
+      end
+
+      # Retrieves a set of commits that have occurred on a branch.
+      # @param branch [Branch] Branch to inspect for commits.
+      # @return [Enumerable<Commit>]
+      def commits_on_branch(branch)
+        raise NotImplementedError
+      end
+
+      # Retrieves a list of all known branches in a repository.
+      # @param repository [Repository] Repository to inspect.
+      # @return [Enumerable<Branch>]
+      def branches_in_repository(repository)
+        raise NotImplementedError
+      end
+
+      private
+      # Generates the path to the local copy of the repository.
+      # @param repository [Repository] Repository to operate on.
+      # @return [String] Path to where the repository (should) reside.
+      def repository_path(repository)
+        repo_dir = Digest::SHA2.hexdigest(repository.url)
+        ::File.join('.', repo_dir)
+      end
+
+      # Generates the Rugged credentials equivalent for the given repository.
+      # @param repository [Repository] Repository to retrieve credentials for.
+      # @return [Object] One of Rugged's supported credential types.
+      def rugged_credentials(repository)
+        case (auth = repository.auth)
+          when nil
+            Rugged::Credentials::Default.new
+          when UserPasswordAuth
+            # Yeah, yeah, I know this is bad.
+            Rugged::Credentials::UserPassword.new(
+                username: auth.instance_variable_get('@username'),
+                password: auth.instance_variable_get('@password'))
+          when SshKeyAuth
+            raise NotImplementedError # Rugged::Credentials::SshKey
+          else
+            raise "Unsupported authentication type for git - #{auth.class}"
+        end
+      end
+
+      # Retrieves the +Rugged::Repository+ instance for a given repository.
+      # @param repository [Repository] Repository to operate on.
+      # @return [Rugged::Repository]
+      def rugged_repository(repository)
+        @cache.lookup(repository) do |repo|
+          path = repository_path(repository)
+          cred = rugged_credentials(repository)
+          Rugged::Repository.clone_at(repo.url, path, credentials: cred)
+        end
+      end
     end
   end
 end
