9593009cbcfb36829c532d4de8f3f997030d08a6
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-06 15:46:30 -0700

    Quickly implement JSON report renderer

--- lib/ferret/reporting/json_report_renderer.rb
+++ lib/ferret/reporting/json_report_renderer.rb
@@ -4,6 +4,17 @@ module Ferret
   module Reporting
     # Transforms report information into a JSON object.
     class JsonReportRenderer < ReportRenderer
+      def render
+        obj = {
+            title:   report.title,
+            time:    report.time,
+            summary: report.summary
+        }
+        report.segments.each do |segment|
+          obj[segment.slug] = segment.to_obj
+        end
+        obj.to_json
+      end
     end
   end
 end
