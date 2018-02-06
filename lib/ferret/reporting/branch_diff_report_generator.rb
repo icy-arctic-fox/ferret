639c4a83355815feec81f4ba97e5ce8069a44732
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-06 15:46:53 -0700

    Trying out branch diff report generator

--- lib/ferret/reporting/branch_diff_report_generator.rb
+++ lib/ferret/reporting/branch_diff_report_generator.rb
@@ -4,6 +4,26 @@ module Ferret
   module Reporting
     # Generates a report for the differences between two branches.
     class BranchDiffReportGenerator < ReportGenerator
+      def initialize(diff)
+        @diff = diff
+        freeze
+      end
+
+      protected
+
+      def segments
+        [
+            CommitListSegment.new('Unmerged commits', 'unmerged', commit_segments(@diff.unmerged_commits))
+        ]
+      end
+
+      private
+
+      def commit_segments(commits)
+        commits.map do |commit|
+          CommitSegment.new(commit)
+        end
+      end
     end
   end
 end
