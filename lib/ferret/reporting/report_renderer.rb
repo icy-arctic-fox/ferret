04c0bc0b56681824cb0a7eadc19e9973d16fc569
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-06 15:46:15 -0700

    Need to store and recall report instance

--- lib/ferret/reporting/report_renderer.rb
+++ lib/ferret/reporting/report_renderer.rb
@@ -3,6 +3,11 @@ module Ferret
     # Creates a human (or machine) consumable output containing report information.
     # @abstract Base class that all report renderers use.
     class ReportRenderer
+      def initialize(report)
+        @report = report
+        freeze
+      end
+
       # @!method render
       #   Generates a string containing the serialized report information.
       #   @return [String] Serialized report information.
@@ -15,6 +20,10 @@ module Ferret
       #   @return [void]
       #   @abstract
       abstract :render_to_file
+
+      protected
+
+      attr_reader :report
     end
   end
 end
