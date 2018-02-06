5b0c41d52af48b062255d40c7323c9bb2cfaadb9
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-06 14:39:55 -0700

    Base implementation for report generator

--- lib/ferret/reporting/report_generator.rb
+++ lib/ferret/reporting/report_generator.rb
@@ -3,6 +3,28 @@ module Ferret
     # Assembles information from various sources to create a report.
     # @abstract Base classes that all report generators use.
     class ReportGenerator
+      # Creates a new report generator.
+      # @param title [String] Name to give the generated report.
+      # @param summary [String] Brief description of the report.
+      def initialize(title, summary)
+        @title   = title
+        @summary = summary
+        freeze
+      end
+
+      # Constructs the report.
+      # @return [Report]
+      def generate
+        Report.new(@title, @summary, segments, Time.now)
+      end
+
+      protected
+
+      # @!method segments
+      #   Generates the segments to be used in the report.
+      #   @return [Enumerable<Segment>]
+      #   @abstract
+      abstract :segments
     end
   end
 end
