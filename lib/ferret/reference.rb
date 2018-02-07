3c9e9ff620e2f97ad7e352ef9be11a3b28d7e5f1
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-07 11:13:51 -0700

    Add attributes and equality check to reference class

--- lib/ferret/reference.rb
+++ lib/ferret/reference.rb
@@ -1,5 +1,17 @@
 module Ferret
   # Points to something related to the code at this point.
   class Reference
+    attr_reader :name
+
+    abstract :url
+
+    def initialize(name)
+      @name = name
+      freeze
+    end
+
+    def ==(other)
+      other.class == self.class && other.name == name
+    end
   end
 end
