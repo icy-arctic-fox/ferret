1464917f6f530ed54e4365d483563864acc9d33e
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-12 15:02:06 -0700

    Ability to render segments (and sub-segments)

--- lib/ferret/reporting/html_report_renderer.rb
+++ lib/ferret/reporting/html_report_renderer.rb
@@ -7,12 +7,15 @@ module Ferret
     class HtmlReportRenderer < ReportRenderer
       def render
         engine = Haml::Engine.new(report_template)
-        engine.render(Object.new, report: report) do |segment|
-          segment.title
-        end
+        engine.render(Object.new, report: report, &method(:render_segment))
       end
 
       private
+
+      def render_segment(segment)
+        segment_engine = Haml::Engine.new(segment_template(segment))
+        segment_engine.render(Object.new, report: report, segment: segment.to_obj, &method(:render_segment))
+      end
 
       def assets_dir
         ::File.join(::File.dirname(__FILE__), '..', '..', '..', 'assets', 'html')
@@ -21,6 +24,19 @@ module Ferret
       def report_template
         report_template_file = ::File.join(assets_dir, 'report.haml')
         ::File.read(report_template_file)
+      end
+
+      def segment_template(segment)
+        snake_case_name = snake_case(segment.class.to_s)
+        segment_template_fie = ::File.join(assets_dir, snake_case_name + '.haml')
+        ::File.read(segment_template_fie)
+      end
+
+      def snake_case(string)
+        name = string.split('::').last
+        name.gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+        gsub(/([a-z\d])([A-Z])/, '\1_\2').
+        tr('-', '_').downcase
       end
     end
   end
