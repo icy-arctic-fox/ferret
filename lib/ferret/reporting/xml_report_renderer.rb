1f428cc75244980f7ac4e56ce0afd9943d5b4c1f
Michael Miller <icy.arctic.fox@gmail.com>
2018-02-15 11:50:30 -0700

    Initial XML report renderer
    Depends on Nokogiri for XML generation.
    Arrays aren't fully handled yet.

--- lib/ferret/reporting/xml_report_renderer.rb
+++ lib/ferret/reporting/xml_report_renderer.rb
@@ -1,9 +1,53 @@
+require 'nokogiri'
 require_relative 'report_renderer'
 
 module Ferret
   module Reporting
     # Transforms report information into XML document.
     class XmlReportRenderer < ReportRenderer
+      def render
+        builder = Nokogiri::XML::Builder.new do |xml|
+          xml.report {
+            xml.title report.title
+            xml.time report.time
+            xml.summary report.summary
+            report.segments.each do |segment|
+              build_segment_xml(xml, segment)
+            end
+          }
+        end
+        builder.to_xml
+      end
+
+      private
+      def build_segment_xml(xml, segment)
+        # This is dumb, either add a method to support this without reflection or update the docs if there is a method.
+        xml.send(segment.slug.to_sym) {
+          xml.title segment.title
+          build_obj_xml(xml, segment.to_obj)
+        }
+      end
+
+      def build_obj_xml(xml, obj)
+        obj.each do |k, v|
+          case v
+            when Hash
+              xml.send(k) {
+                build_obj_xml(xml, v)
+              }
+            when Array
+              xml.send(k) {
+                v.each do |i|
+                  xml.send(:entry) {# TODO: Replace with singular form of k
+                    build_obj_xml(xml, i)
+                  }
+                end
+              }
+            else
+              xml.send(k, v)
+          end
+        end
+      end
     end
   end
 end
